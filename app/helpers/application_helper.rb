0c45410f0e0b4697755b718c36e55c69860a9f79
MrJoshE <josheverett02@gmail.com>
2021-12-07 15:32:20 +0000

    Final changes to readme

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def on_home_page
-    return false if params[:controller] == 'pages' && params[:action] == 'index'
+    return false if (params[:controller] == 'pages' && params[:action] == 'index') || (params[:controller] == 'registrations' && params[:action] == 'index') || (params[:controller] == 'pages' && params[:action] == 'about')
     true
   end
 end
